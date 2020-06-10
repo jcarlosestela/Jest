3c4b26cd7be4c7091478e8a124dae2eb3463b4ea
Jose Carlos Estela <jose.carlos.estela@ciberexperis.es>
2020-06-10 21:57:46 +0200

    UPDATE: Dispatch

--- Sources/Jest/Domain/UseCase.swift
+++ Sources/Jest/Domain/UseCase.swift
@@ -5,3 +5,10 @@ public protocol UseCase {
     associatedtype Output: Decodable
     func does(_ input: Input) throws -> Output
 }
+
+extension UseCase {
+    
+    public func schedule(on scheduler: UseCaseScheduler, input: Input, completion: @escaping (Result<Output, Error>) -> Void) {
+        scheduler.schedule(self, input: input, completion: completion)
+    }
+}
