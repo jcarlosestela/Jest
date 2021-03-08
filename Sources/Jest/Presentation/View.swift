cd95877d26b5c5350351c5b1da5c442558074cae
Jose Carlos Estela <jose.carlos.estela@ciberexperis.es>
2021-03-08 20:53:30 +0100

    REmove file

--- Sources/Jest/Presentation/View.swift
+++
@@ -1,9 +0,0 @@
-//
-//  View.swift
-//  Jest
-//
-//  Created by José Carlos Estela Anguita on 15/05/2020.
-//
-
-public protocol View: class, DialogPresentationCapable, LoadingPresentationCapable {}
-
